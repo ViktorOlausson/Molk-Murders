843275cb039c8ed301b966b7c7ee1632d67776e1
korvkioscar <oscarmartensson1@gmail.com>
2024-02-28 14:51:52 +0100

    Punkter.

--- MolkMurdersSystem/EventRegister.cs
+++ MolkMurdersSystem/EventRegister.cs
@@ -13,24 +13,24 @@ namespace MolkMurdersSystem {
 
         public static void Register() {
             // It would also be possible to make the text inside PlainEvent completely random. Priority would still matter.
-            Events.Add(new PlainEvent("%PLAYER1% gick vilse i skogen i exakt två timmar"));
-            Events.Add(new PlainEvent("%PLAYER1% missade bussen precis och fick vänta en timme"));
-            Events.Add(new PlainEvent("%PLAYER1% fastnade i en while loop"));
-            Events.Add(new PlainEvent("%PLAYER1% stod mest omkring alldeles ensam"));
-            Events.Add(new PlainEvent("%PLAYER1% fastnade hos optikern"));
-            Events.Add(new PlainEvent("%PLAYER1% trasslade in sig i sina hörlurskablar och kunde inte komma loss"));
+            Events.Add(new PlainEvent("%PLAYER1% gick vilse i skogen i exakt två timmar."));
+            Events.Add(new PlainEvent("%PLAYER1% missade bussen precis och fick vänta en timme."));
+            Events.Add(new PlainEvent("%PLAYER1% fastnade i en while loop."));
+            Events.Add(new PlainEvent("%PLAYER1% stod mest omkring alldeles ensam."));
+            Events.Add(new PlainEvent("%PLAYER1% fastnade hos optikern."));
+            Events.Add(new PlainEvent("%PLAYER1% trasslade in sig i sina hörlurskablar och kunde inte komma loss."));
             Events.Add(new PlainEvent("%PLAYER1% ville hellre lära sig ett nytt språk, danska kanske?"));
-            Events.Add(new PlainEvent("%PLAYER1% sprang och gömde sig i en buske"));
-            Events.Add(new PlainEvent("%PLAYER1% fick en snilleblixt"));
-            Events.Add(new PlainEvent("%PLAYER1% fick en tindermatchning och sprang på date istället"));
-            Events.Add(new PlainEvent("%PLAYER1% blev arg och hungrig på samma gång"));
-            Events.Add(new PlainEvent("%PLAYER1% laddade sin hagelbrakare"));
-            Events.Add(new PlainEvent("%PLAYER1% torkade svetten från pannan och såg ut över arenan"));
-            Events.Add(new PlainEvent("%PLAYER1% började gråta lite grann"));
-            Events.Add(new PlainEvent("%PLAYER1% tog en tupplur, man blir trött av våld"));
-            Events.Add(new PlainEvent("%PLAYER1% hittade ett annorlunda bär, åt det och kunde inte sluta dansa") { Conditions = [new IsFirstRoundCondition()] });
-            Events.Add(new PlainEvent("%PLAYER1% fann en magiskt spegel och mötte sitt alter ego") { Conditions = [new IsFirstRoundCondition()] });
-            Events.Add(new PlainEvent("%PLAYER1% fick en önskning uppfylld av en vänlig och magisk pensionär vid namn Bert-Rikard") { Conditions = [new IsFirstRoundCondition()] });
+            Events.Add(new PlainEvent("%PLAYER1% sprang och gömde sig i en buske."));
+            Events.Add(new PlainEvent("%PLAYER1% fick en snilleblixt."));
+            Events.Add(new PlainEvent("%PLAYER1% fick en tindermatchning och sprang på date istället."));
+            Events.Add(new PlainEvent("%PLAYER1% blev arg och hungrig på samma gång."));
+            Events.Add(new PlainEvent("%PLAYER1% laddade sin hagelbrakare."));
+            Events.Add(new PlainEvent("%PLAYER1% torkade svetten från pannan och såg ut över arenan."));
+            Events.Add(new PlainEvent("%PLAYER1% började gråta lite grann."));
+            Events.Add(new PlainEvent("%PLAYER1% tog en tupplur, man blir trött av våld."));
+            Events.Add(new PlainEvent("%PLAYER1% hittade ett annorlunda bär, åt det och kunde inte sluta dansa.") { Conditions = [new IsFirstRoundCondition()] });
+            Events.Add(new PlainEvent("%PLAYER1% fann en magiskt spegel och mötte sitt alter ego.") { Conditions = [new IsFirstRoundCondition()] });
+            Events.Add(new PlainEvent("%PLAYER1% fick en önskning uppfylld av en vänlig och magisk pensionär vid namn Bert-Rikard.") { Conditions = [new IsFirstRoundCondition()] });
 
 
             // Condition tests
@@ -38,8 +38,8 @@ namespace MolkMurdersSystem {
             Events.Add(new PlainEvent("%PLAYER1% vaknade och fann sig själv i mitten av arenan.") { Conditions = [new IsFirstRoundCondition()] });
             Events.Add(new PlainEvent("%PLAYER1% använde sig av en uråldrig besvärjelse och hamnade på arenan.") { Conditions = [new IsFirstRoundCondition()] });
             Events.Add(new PlainEvent("%PLAYER1% gick vilse när han skulle till hemköp och gick rätt in på arenan.") { Conditions = [new IsFirstRoundCondition()] });
-            Events.Add(new PlainEvent("%PLAYER1% gick självsäkert in på mitten av arenan, solens strålar sken mot svärdets blad. Nu var det dags") { Conditions = [new IsFirstRoundCondition()] });
-            Events.Add(new PlainEvent("%PLAYER1% gick in på arenan och såg sig oroligt över axeln") { Conditions = [new IsFirstRoundCondition()] });
+            Events.Add(new PlainEvent("%PLAYER1% gick självsäkert in på mitten av arenan, solens strålar sken mot svärdets blad. Nu var det dags.") { Conditions = [new IsFirstRoundCondition()] });
+            Events.Add(new PlainEvent("%PLAYER1% gick in på arenan och såg sig oroligt över axeln.") { Conditions = [new IsFirstRoundCondition()] });
 
 
             // RandomPlainEvent tests
@@ -59,7 +59,7 @@ namespace MolkMurdersSystem {
             Events.Add(new TragicAccidentEvent("%PLAYER1% fick trots extrema odds en meteor i skallen."));
             Events.Add(new TragicAccidentEvent("%PLAYER1% ville praktiskt lära sig hur seppuku gick till."));
             Events.Add(new TragicAccidentEvent("%PLAYER1% försökte lära sig hur doxygen fungerar."));
-            Events.Add(new TragicAccidentEvent("%PLAYER1% fick en fiskmås i ögat"));
+            Events.Add(new TragicAccidentEvent("%PLAYER1% fick en fiskmås i ögat."));
 
         }
 
