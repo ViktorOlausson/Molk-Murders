b78cc6defe125b7971a14c4f98ababb7586eeee7
dunderhugo <forssellhugo@gmail.com>
2024-02-29 13:08:20 +0100

    FIXME % TODO kommentarer

--- MolkMurders/Views/music.xaml.cs
+++ MolkMurders/Views/music.xaml.cs
@@ -1,9 +1,5 @@
-using Microsoft.UI.Xaml;
 namespace MolkMurders.Views;
 using System;
-using System.Drawing;
-using System.Security.Cryptography.X509Certificates;
-using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Core.Primitives;
 
 public partial class music
@@ -14,23 +10,19 @@ public partial class music
 
     }
 
-
     private void doneBtn_Clicked(object sender, EventArgs e)
     {
         Close();
     }
 
-
-
-    void OnPlayPauseButtonClicked(object sender, EventArgs args)
+	void OnPlayPauseButtonClicked(object sender, EventArgs args)
     {
+
         if (mediaElement.CurrentState == MediaElementState.Stopped ||
             mediaElement.CurrentState == MediaElementState.Paused)
         {
             mediaElement.Play();
             PlayPause.Text = "PÅ";
-
-
         }
         else if (mediaElement.CurrentState == MediaElementState.Playing)
         {
